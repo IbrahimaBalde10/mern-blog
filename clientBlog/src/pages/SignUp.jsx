ac15f722c0ea30ce7e2977e12fa389a3ced00acb
BALDE Ibrahima <ibraima.balde21@estm.edu.sn>
2024-05-09 16:09:19 +0000

    creation de UI d'inscription

--- clientBlog/src/pages/SignUp.jsx
+++ clientBlog/src/pages/SignUp.jsx
@@ -1,7 +1,49 @@
+import { Button, Label, TextInput } from 'flowbite-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
-    return <div><h3>SignUp</h3></div>;
+    return <div className='min-h-screen mt-20'>
+        <div className=' flex p-3 max-w-3xl mx-auto flex-col md:flex-row 
+        md:items-center gap-5'>
+            {/* guache */}
+            <div className='flex-1'>
+            <Link to="/" className='whitespace-nowrap 
+                text-sm sm:text-xl font-bold dark:text-white'>
+                <span className='px-2 py-2 bg-gradient-to-r from-indigo-500
+                via-purple-500 to-pink-500 rounded-lg text-white' >BALDEV'S</span>
+                Blog
+            </Link>
+            <p className='text-sm mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Explicabo.</p>
+            </div>
+            {/* droite */}
+            <div className='flex-1'>
+                <form className='flex flex-col gap-4'>
+                    <div>
+                       <Label value='Your username'/>
+                       <TextInput type='text' placeholder='Username' id='username' />
+                    </div>
+                    <div>
+                       <Label value='Your Email'/>
+                       <TextInput type='email' placeholder='name@company.com' id='email' />
+                    </div>
+                    <div>
+                       <Label value='Your Password'/>
+                       <TextInput type='password' placeholder='password' id='password' />
+                    </div>
+                    <Button gradientDuoTone='purpleToPink' type='submit'>
+                        Singn Up
+                    </Button>
+                </form>
+                <div className='flex gap-2 text-sm mt-5'>
+                    <span>Have an account?</span>
+                    <Link to='/singn-in'> Sign In</Link>
+                </div>
+            </div>
+        </div>
+        
+    </div>
 }
 
 
