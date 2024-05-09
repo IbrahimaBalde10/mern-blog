3465277dcd5e68bfa78b332770bd5c2db24087fc
BALDE Ibrahima <ibraima.balde21@estm.edu.sn>
2024-05-09 00:25:57 +0000

    creation dUne fonction pour les erreurs

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,15 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async(req, res)=>{
+export const signup = async(req, res, next)=>{
     // console.log(req.body);
     const { username, email, password} = req.body;
     const hashPassword = bcryptjs.hashSync(password, 10);
 
     if(!username || !email || !password || username === '' || email === '' || password === '' ){
-        return res.status(400).json({message: 'Tous les champs sont obligatoires'});
+       next(errorHandler(400, 'Tous les champs sont obligatoires'));
+        // return res.status(400).json({message: 'Tous les champs sont obligatoires'});
     }
 
     const newUser = new User(
@@ -17,11 +19,10 @@ export const signup = async(req, res)=>{
             password: hashPassword,
         });
     try {
-        await newUser.save();    
+        await newUser.save();   
+        res.json({message:'Inscription reussi!!!!!'}); 
     } catch (error) {
-        res.status(500).json({message: error.message});
+        // res.status(500).json({message: error.message});
+        next(error);
     }
-
-    
-    res.json({message:'Inscription reussi!!!!!'});
-}
+};
